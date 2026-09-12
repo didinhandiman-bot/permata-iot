0f62927bbf6d5054bbbc18f0a1eb78673f2f07a4
Didin Handiman <didinhandiman@gmail.com>
2026-09-12 22:02:34 +0700

    fix: use @vite blade directive for CSS/JS instead of hardcoded paths

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,8 +7,7 @@
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="/build/assets/app-jrvPxRVJ.css">
-    <script src="/build/assets/app-DevHqigE.js" defer></script>
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 <body class="bg-slate-950 text-slate-200 font-inter antialiased min-h-screen">
 
